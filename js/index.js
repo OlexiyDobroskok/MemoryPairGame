84c79edb9785fe2305c11bdca291b1be905ac090
Olexiy Dobroskok <a.v.dobroskok.kh@gmail.com>
2022-09-07 21:45:45 +0300

    Add animation and some style for page

--- js/index.js
+++ js/index.js
@@ -49,10 +49,23 @@ const deckMoneyHeist = [
   },
 ];
 
+const selectedCards = [];
+let previousCard;
+const maxPairs = 6;
+let numberOfPairs = 0;
+let sumFlippedCards = 0;
+
 const wrap = document.querySelector(".wrap");
 const playArea = document.createElement("ul");
 playArea.classList.add("card__list");
 wrap.append(playArea);
+const pairsInfo = document.createElement("p");
+pairsInfo.classList.add("text__info");
+wrap.before(pairsInfo);
+
+function showPairs() {
+  pairsInfo.innerHTML = `Pairs found: ${numberOfPairs} / ${maxPairs}`;
+}
 
 function shuffle(deck) {
   return deck.sort(function () {
@@ -61,6 +74,7 @@ function shuffle(deck) {
 }
 
 function newGame() {
+  showPairs();
   let gameDeck = shuffle(deckMoneyHeist);
   playArea.append(...makePlayArea(gameDeck));
 }
@@ -70,11 +84,13 @@ function makePlayArea(deck) {
     const gameCard = document.createElement("li");
     gameCard.classList.add("card__item");
     const openCard = document.createElement("img");
-    openCard.classList.add("card__img", "close__card");
+    openCard.classList.add("opened__card");
+    openCard.draggable = false;
     openCard.src = card.openCard;
     gameCard.append(openCard);
     const closeCard = document.createElement("img");
-    closeCard.classList.add("card__img");
+    closeCard.classList.add("closed__card");
+    closeCard.draggable = false;
     closeCard.src = card.closeCard;
     gameCard.append(closeCard);
     gameCard.addEventListener("click", searchPair);
@@ -82,17 +98,11 @@ function makePlayArea(deck) {
   });
 }
 
-const selectedCards = [];
-let previousCard;
-let numberOfPairs = 0;
-
 function searchPair({ target }) {
   let chosenCard = target.closest("li");
-  if (previousCard !== chosenCard) {
-    for (let statusCard of chosenCard.children) {
-      statusCard.classList.toggle("close__card");
-    }
-    chosenCard.classList.add("open__card");
+  if (previousCard !== chosenCard && sumFlippedCards < 2) {
+    chosenCard.classList.add("flipped");
+    sumFlippedCards++;
     if (selectedCards.length < 2) {
       selectedCards.push(chosenCard.firstChild.src);
       if (selectedCards.length === 2 && selectedCards[0] === selectedCards[1]) {
@@ -101,51 +111,59 @@ function searchPair({ target }) {
         selectedCards.length === 2 &&
         selectedCards[0] !== selectedCards[1]
       ) {
-        chosenCard = "";
         resetPairCards();
+        chosenCard = "";
       }
     }
+    previousCard = chosenCard;
   }
-  previousCard = chosenCard;
+
   resetGame();
 }
 
 function getPairCards() {
   numberOfPairs++;
-  for (let gameCard of playArea.children) {
-    if (gameCard.classList.contains("open__card")) {
-      gameCard.classList.remove("open__card");
-      gameCard.removeEventListener("click", searchPair);
-      selectedCards.length = 0;
+  showPairs();
+  selectedCards.length = 0;
+  setTimeout(() => {
+    for (let gameCard of playArea.children) {
+      if (gameCard.classList.contains("flipped")) {
+        gameCard.classList.remove("flipped");
+        gameCard.classList.add("check");
+        gameCard.removeEventListener("click", searchPair);
+        sumFlippedCards = 0;
+        while (gameCard.firstChild) {
+          gameCard.removeChild(gameCard.firstChild);
+        }
+      }
     }
-  }
+  }, 800);
 }
 
 function resetPairCards() {
   selectedCards.length = 0;
-  for (let gameCard of playArea.children) {
-    if (gameCard.classList.contains("open__card")) {
-      gameCard.classList.remove("open__card");
-      setTimeout(() => {
-        for (let statusCard of gameCard.children) {
-          statusCard.classList.toggle("close__card");
-        }
-      }, 500);
+  setTimeout(() => {
+    for (let gameCard of playArea.children) {
+      if (gameCard.classList.contains("flipped")) {
+        gameCard.classList.remove("flipped");
+        sumFlippedCards = 0;
+      }
     }
-  }
+  }, 800);
 }
 
 function resetGame() {
-  if (numberOfPairs === 6) {
+  if (numberOfPairs === maxPairs) {
     setTimeout(() => {
       selectedCards.length = 0;
       previousCard = "";
       numberOfPairs = 0;
+      sumFlippedCards = 0;
       while (playArea.firstChild) {
         playArea.removeChild(playArea.firstChild);
       }
       newGame();
-    }, 1000);
+    }, 3000);
   }
 }
 
